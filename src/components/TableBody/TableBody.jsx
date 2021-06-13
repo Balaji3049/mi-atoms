ec91e95c79371a4e04db4d820b5f792401029235
Balaji <balaji3049@gmail.com>
2021-06-13 17:48:53 +0530

    Fixed bug in tablebody component ...

--- src/components/TableBody/TableBody.jsx
+++ src/components/TableBody/TableBody.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import MUITableBody from "@material-ui/core/TableBody";
 
 export const TableBody = (props) => {
-    <>
-        <MUITableBody {...props} />
-    </>;
+    return (
+        <>
+            <MUITableBody {...props} />
+        </>
+    );
 };
