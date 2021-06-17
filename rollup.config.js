4134cb4d7464ef2d2445d8eb10e7cbbb4d78470c
Balaji <balaji3049@gmail.com>
2021-06-17 15:20:51 +0530

    Fixing workflow issue ...

--- rollup.config.js
+++ rollup.config.js
@@ -72,7 +72,7 @@ export default [
             },
             /* {
                 file: "dist/bundle.js",
-                format: "iife",
+                //format: "iife",
                 //sourcemap: true,
             }, */
             {
